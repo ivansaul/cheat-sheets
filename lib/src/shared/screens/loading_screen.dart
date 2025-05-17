e658adcaf07c0efab40d4ebca8aa55b8d448775d
ivansaul <saul_2503@hotmail.com>
2025-05-16 20:04:08 -0500

    refactor: introduce AppException class and enhance error and loading screens

--- lib/src/shared/screens/loading_screen.dart
+++ lib/src/shared/screens/loading_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({super.key});
+class LoadingView extends StatelessWidget {
+  const LoadingView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const LoadingView(),
     );
   }
 }
