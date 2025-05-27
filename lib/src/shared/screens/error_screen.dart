6826356e49540775a6854be49a512f22cb567808
ivansaul <saul_2503@hotmail.com>
2025-05-27 00:47:23 -0500

    refactor: improve error handling display

--- lib/src/shared/screens/error_screen.dart
+++ lib/src/shared/screens/error_screen.dart
@@ -1,20 +1,6 @@
+import 'package:cheat_sheets/src/shared/exceptions/app_exceptions.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-class ErrorView extends StatelessWidget {
-  const ErrorView({
-    super.key,
-    required this.message,
-  });
-
-  final String message;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(message),
-    );
-  }
-}
 
 class ErrorScreen extends StatelessWidget {
   final String message;
@@ -31,5 +17,74 @@ class ErrorScreen extends StatelessWidget {
         child: Text(message),
       ),
     );
+  }
+}
+
+class ErrorView extends StatelessWidget {
+  const ErrorView({
+    super.key,
+    required this.error,
+    this.stackTrace,
+  });
+
+  final Object error;
+  final StackTrace? stackTrace;
+
+  @override
+  Widget build(BuildContext context) {
+    return kDebugMode
+        ? _DebugView(
+            error: error,
+            stackTrace: stackTrace,
+          )
+        : Center(
+            child: Text(_message),
+          );
+  }
+
+  String get _message {
+    if (error is AppException) {
+      return (error as AppException).message();
+    }
+    return const AppException.unknown().message();
+  }
+}
+
+class _DebugView extends StatelessWidget {
+  const _DebugView({
+    required this.error,
+    this.stackTrace,
+  });
+
+  final Object error;
+  final StackTrace? stackTrace;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Text(_message),
+        if (_error != null) Text("\nError:\n$_error"),
+        if (_stackTrace != null) Text("\nStack Trace:\n$_stackTrace"),
+        if (stackTrace != null)
+          Text("\nLast Stack Trace:\n${stackTrace.toString()}"),
+      ],
+    );
+  }
+
+  AppException? get _appException {
+    return error is AppException ? error as AppException : null;
+  }
+
+  String get _message {
+    return _appException?.message() ?? error.toString();
+  }
+
+  String? get _error {
+    return _appException?.error.toString();
+  }
+
+  String? get _stackTrace {
+    return _appException?.stackTrace?.toString();
   }
 }
--- lib/src/shared/screens/loading_screen.dart
+++ lib/src/shared/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoadingView extends StatelessWidget {
@@ -6,7 +7,7 @@ class LoadingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CircularProgressIndicator(),
+      child: CupertinoActivityIndicator(),
     );
   }
 }
