3a891ef6ab0243a2388c90e47fca1196c438fcff
ivansaul <saul_2503@hotmail.com>
2026-02-02 04:46:25 -0500

    refactor: env variable selection to helper function

--- lib/src/shared/env.dart
+++ lib/src/shared/env.dart
@@ -6,41 +6,48 @@ part 'env.g.dart';
 @Envied(path: '.env', name: 'ProductionEnv', obfuscate: true)
 @Envied(path: '.env_debug', name: 'DebugEnv', obfuscate: false)
 abstract interface class Env {
+  static T _getEnvValue<T>(T debugValue, T prodValue) {
+    return kDebugMode ? debugValue : prodValue;
+  }
+
+  static final _debugEnv = _DebugEnv();
+  static final _prodEnv = _ProductionEnv();
+
   // Coupon
   @EnviedField(varName: 'API_COUPON_BASE_URL')
-  static final String apiCouponBaseUrl = switch (kDebugMode) {
-    true => _DebugEnv().apiCouponBaseUrl,
-    false => _ProductionEnv().apiCouponBaseUrl,
-  };
+  static final String apiCouponBaseUrl = _getEnvValue(
+    _debugEnv.apiCouponBaseUrl,
+    _prodEnv.apiCouponBaseUrl,
+  );
 
   @EnviedField(varName: 'API_COUPON_LIST_ENDPOINT')
-  static final String apiCouponListEndpoint = switch (kDebugMode) {
-    true => _DebugEnv().apiCouponListEndpoint,
-    false => _ProductionEnv().apiCouponListEndpoint,
-  };
+  static final String apiCouponListEndpoint = _getEnvValue(
+    _debugEnv.apiCouponListEndpoint,
+    _prodEnv.apiCouponListEndpoint,
+  );
 
   @EnviedField(varName: 'API_COUPON_ENDPOINT')
-  static final String apiCouponEndpoint = switch (kDebugMode) {
-    true => _DebugEnv().apiCouponEndpoint,
-    false => _ProductionEnv().apiCouponEndpoint,
-  };
+  static final String apiCouponEndpoint = _getEnvValue(
+    _debugEnv.apiCouponEndpoint,
+    _prodEnv.apiCouponEndpoint,
+  );
 
   // Cheatsheets
   @EnviedField(varName: 'API_CHEATSHEET_BASE_URL')
-  static final String apiCheatsheetBaseUrl = switch (kDebugMode) {
-    true => _DebugEnv().apiCheatsheetBaseUrl,
-    false => _ProductionEnv().apiCheatsheetBaseUrl,
-  };
+  static final String apiCheatsheetBaseUrl = _getEnvValue(
+    _debugEnv.apiCheatsheetBaseUrl,
+    _prodEnv.apiCheatsheetBaseUrl,
+  );
 
   @EnviedField(varName: 'API_CHEATSHEET_LIST_ENDPOINT')
-  static final String apiCheatsheetListEndpoint = switch (kDebugMode) {
-    true => _DebugEnv().apiCheatsheetListEndpoint,
-    false => _ProductionEnv().apiCheatsheetListEndpoint,
-  };
+  static final String apiCheatsheetListEndpoint = _getEnvValue(
+    _debugEnv.apiCheatsheetListEndpoint,
+    _prodEnv.apiCheatsheetListEndpoint,
+  );
 
   @EnviedField(varName: 'API_CHEATSHEET_DETAIL_ENDPOINT')
-  static final String apiCheatsheetDetailEndpoint = switch (kDebugMode) {
-    true => _DebugEnv().apiCheatsheetDetailEndpoint,
-    false => _ProductionEnv().apiCheatsheetDetailEndpoint,
-  };
+  static final String apiCheatsheetDetailEndpoint = _getEnvValue(
+    _debugEnv.apiCheatsheetDetailEndpoint,
+    _prodEnv.apiCheatsheetDetailEndpoint,
+  );
 }
